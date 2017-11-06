af400187a3a4cdea5809aa6c32ccc030c0930821
luc-vdp <luc-vdp@users.noreply.github.com>
2017-11-06 13:31:10 +0100

    pydex - attempt to work with class
    NOT OK.

--- pydex.py
+++ pydex.py
@@ -8,18 +8,18 @@ Created on Sun Nov  5 12:59:18 2017
 """
 
 import tkinter as tk
-#import tkinter.ttk as ttk
+import tkinter.ttk as ttk
 import tkinter.messagebox as messagebox
 #from tkinter import messagebox
 from tkinter import filedialog
 
-#import pandas as pd
+import pandas as pd
 #import seaborn as sb
 
 #from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg #, NavigationToolbar2TkAgg
 #from matplotlib.figure import Figure
 
-class PyDex:
+class PyDex(ttk.Frame):
     def __init__(self, master):
         self.master = master
         master.title("PyDex - Python Data Exploration Tool - version 0.1")
@@ -32,30 +32,30 @@ class PyDex:
         self.menuFile = tk.Menu(self.menuMain, tearoff=False)
         self.menuMain.add_cascade(label="File", menu=self.menuFile)
         self.menuFile.add_command(label="New", command=self.NewFile)
-#        self.menuFile.add_command(label="Open...", command=self.OpenFile)
-#        self.menuFile.add_separator()
-#        self.menuFile.add_command(label="Exit", command=root.destroy)
-#        
-#        self.menuEdit = tk.Menu(self.menuMain, tearoff=False)
-#        self.menuMain.add_cascade(label="Edit", menu=self.menuEdit)
-#        self.menuEdit.add_command(label="Rename...", command=self.RenameDataframe)
-#        
-#        self.menuTools = tk.Menu(self.menuMain, tearoff=False)
-#        self.menuMain.add_cascade(label="Tools", menu=self.menuTools)
-#        self.menuTools.add_command(label="Preferences...", command=self.Preferences)
-#        
-#        self.menuHelp = tk.Menu(self.menuMain, tearoff=False)
-#        self.menuMain.add_cascade(label="Help", menu=self.menuHelp)
-#        self.menuHelp.add_command(label="About...", command=self.About)
-#
-#        # Left frame
-#        self.frameLeft = ttk.Frame(master)
-#        self.frameLeft.pack(side=tk.LEFT, anchor=tk.NW, fill=tk.Y, padx=4, pady=2)
-#        
-#        # Combobox to select a data frame
-#        self.combobox = ttk.Combobox(self.frameLeft)
-#        variables= [var for var in dir() if isinstance(eval(var), pd.core.frame.DataFrame)]
-#        self.combobox['values'] = variables
+        self.menuFile.add_command(label="Open...", command=self.OpenFile)
+        self.menuFile.add_separator()
+        self.menuFile.add_command(label="Exit", command=root.destroy)
+        
+        self.menuEdit = tk.Menu(self.menuMain, tearoff=False)
+        self.menuMain.add_cascade(label="Edit", menu=self.menuEdit)
+        self.menuEdit.add_command(label="Rename...", command=self.RenameDataframe)
+        
+        self.menuTools = tk.Menu(self.menuMain, tearoff=False)
+        self.menuMain.add_cascade(label="Tools", menu=self.menuTools)
+        self.menuTools.add_command(label="Preferences...", command=self.Preferences)
+        
+        self.menuHelp = tk.Menu(self.menuMain, tearoff=False)
+        self.menuMain.add_cascade(label="Help", menu=self.menuHelp)
+        self.menuHelp.add_command(label="About...", command=self.About)
+
+        # Left frame
+        self.frameLeft = ttk.Frame(self.master)
+        self.frameLeft.pack(side=tk.LEFT, anchor=tk.NW, fill=tk.Y, padx=4, pady=2)
+        
+        # Combobox to select a data frame
+        self.combobox = ttk.Combobox(self.frameLeft)
+        variables= [var for var in dir() if isinstance(eval(var), pd.core.frame.DataFrame)]
+        self.combobox['values'] = variables
 #        #self.combobox.bind('<Return>',ListColumns)
 #        #self.combobox.bind('<<ComboboxSelected>>',ListColumns)
 #        self.combobox.pack(pady=4)
